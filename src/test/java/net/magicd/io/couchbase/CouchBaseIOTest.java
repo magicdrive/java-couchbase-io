d3c077c276732ee4e1580592fa9f9c4150647368
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2014-05-27 00:04:18 +0900

    test update

--- src/test/java/net/magicd/io/couchbase/CouchBaseIOTest.java
+++ src/test/java/net/magicd/io/couchbase/CouchBaseIOTest.java
@@ -1,20 +1,49 @@
 package net.magicd.io.couchbase;
 
 import net.magicd.io.couchbase.mock.TestData;
+import org.ho.yaml.Yaml;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class CouchBaseIOTest {
 
-    static CouchBaseIO aio;
+    private CouchBaseIO io;
 
-    static {
+    private static void p(Object obj) {
+        System.err.println(obj.toString());
+    }
+
+    @Before
+    public void init() {
+
         try {
-            aio = new CouchBaseIO(
-                    "http://ubuntu-lts.local:8091/pools",
+            byte[] fileContentBytes = Files.readAllBytes(
+                    Paths.get(
+                            getClass().getClassLoader().getResource("couchbase.yaml").getPath()
+                    )
+            );
+            String fileContentStr = new String(fileContentBytes, StandardCharsets.UTF_8);
+
+
+            Map<String, String> config = (Map<String, String>) (
+                    (Map) Yaml.load(fileContentStr)
+            ).get("endpoint");
+
+            String protocol = config.get("protocol"),
+                   host = config.get("host"),
+                   port = config.get("port"),
+                   path = config.get("path");
+
+            io = new CouchBaseIO(
+                    String.format("%s://%s:%s%s", protocol, host, port, path),
                     "default",
                     ""
             );
@@ -35,8 +64,8 @@ public class CouchBaseIOTest {
                 return this;
             }
         }.init();
-        aio.put("foo", td);
-        TestData result = aio.get("foo", TestData.class);
+        io.put("foo", td);
+        TestData result = io.get("foo", TestData.class);
         assertEquals(td.getId(), result.getId());
         assertEquals(td.getName(), result.getName());
     }
